3419539ee24b8e8d7f301367b8fe37bedb777a02
Benedikt Deicke <benedikt@benediktdeicke.com>
2024-08-30 17:09:53 +0200

    Adds very basic implementation of #find, #find_by, #where, #offset, #limit, and #size

--- lib/active_model/relation.rb
+++ lib/active_model/relation.rb
@@ -5,8 +5,73 @@ require 'active_model'
 module ActiveModel
   # = Active Model Relation
   class Relation
-    def initialize; end
+    include Enumerable
 
-    # TODO: Implement everything
+    attr_reader :model
+
+    delegate :each, :size, :last, to: :records
+
+    def initialize(model, records = [])
+      @model = model
+      @records = records
+      @where_clause = {}
+    end
+
+    def find(value)
+      primary_key = model.try(:primary_key) || :id
+
+      records.find { |record| record.public_send(primary_key) == value }
+    end
+
+    def find_by(...)
+      where(...).first
+    end
+
+    def where(...)
+      spawn.where!(...)
+    end
+
+    def where!(attributes)
+      @where_clause.merge!(attributes)
+
+      self
+    end
+
+    def offset(...)
+      spawn.offset!(...)
+    end
+
+    def offset!(offset)
+      @offset = offset
+
+      self
+    end
+
+    def limit(...)
+      spawn.limit!(...)
+    end
+
+    def limit!(limit)
+      @limit = limit
+
+      self
+    end
+
+    def to_ary
+      records.dup
+    end
+
+    def records
+      @records
+        .select { |record| @where_clause.all? { |key, value| record.public_send(key) == value } }
+        .drop(@offset || 0)
+        .take(@limit || @records.size)
+    end
+
+    private
+
+    def spawn
+      clone
+    end
   end
 end
--- spec/active_model/relation_spec.rb
+++ spec/active_model/relation_spec.rb
@@ -2,8 +2,99 @@
 
 require 'active_model/relation'
 
+class Project
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attribute :id, :integer
+  attribute :state, :string, default: :draft
+end
+
 RSpec.describe ActiveModel::Relation do
   it 'has a version number' do
     expect(ActiveModel::Relation::VERSION).not_to be nil
+  end
+
+  let(:model_class) { Project }
+  let(:records) do
+    [
+      model_class.new(id: 1, state: 'draft'),
+      model_class.new(id: 2, state: 'running'),
+      model_class.new(id: 3, state: 'completed')
+    ]
+  end
+
+  subject { described_class.new(model_class, records) }
+
+  describe '#model' do
+    it 'should return the model class' do
+      expect(subject.model).to eq(model_class)
+    end
+  end
+
+  describe '#find' do
+    it 'should return the record by primary key' do
+      expect(subject.find(1)).to eq(records[0])
+    end
+  end
+
+  describe '#find_by' do
+    it 'should return the record matching the given attributes' do
+      expect(subject.find_by(state: 'running')).to eq(records[1])
+    end
+  end
+
+  describe '#size' do
+    it 'should return the number of records' do
+      expect(subject.size).to eq(3)
+    end
+
+    context 'when the records are filtered' do
+      it 'should return the number of filtered records' do
+        expect(subject.where(state: 'completed').size).to eq(1)
+      end
+    end
+  end
+
+  describe '#where' do
+    it 'should return a new relation' do
+      expect(subject.where(state: 'completed')).to be_a(described_class)
+    end
+
+    it 'should return a new instance' do
+      expect(subject.where(state: 'completed')).not_to eq(subject)
+    end
+
+    it 'should filter the records' do
+      expect(subject.where(state: 'completed')).to match_array([records[2]])
+    end
+  end
+
+  describe '#offset' do
+    it 'should return a new relation' do
+      expect(subject.offset(1)).to be_a(described_class)
+    end
+
+    it 'should return a new instance' do
+      expect(subject.offset(1)).not_to eq(subject)
+    end
+
+    it 'should offset the records' do
+      expect(subject.offset(1)).to match_array(records[1..2])
+    end
+  end
+
+  describe '#limit' do
+    it 'should return a new relation' do
+      expect(subject.limit(1)).to be_a(described_class)
+    end
+
+    it 'should return a new instance' do
+      expect(subject.limit(1)).not_to eq(subject)
+    end
+
+    it 'should limit the records' do
+      expect(subject.limit(1)).to match_array(records[0..0])
+    end
   end
 end
